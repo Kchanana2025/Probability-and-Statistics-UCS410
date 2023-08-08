9b3808db5d62a6c7e35fe2f702e1927e0cb8c58a
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-08 14:36:22 +0530

    Update Assignment_1.R

--- Assignment_1.R
+++ Assignment_1.R
@@ -17,15 +17,48 @@ rm(c) #to remove any variable from the environment
 n=as.integer(readline())#first you need to input the integer 
 factorial(n)#then you can call factorial function(yyou cant do both together as it will cause error)
 #OR
-n=as.integer(readline(prompt="Enter value of n"))
+n=as.integer(readline(prompt="Enter value of n whos factorial you wish to calculate"))
 factorial=1
 if(n<0){
   print("The number is negative")
 }else if(n==0)#else if cant start from a new line
   {
   print("factorial is 1")
-}else{
+}else{#else cant start from a new line 
   for(i in 1:n)
   factorial=factorial*i
   print(factorial)
 }
+
+#Q3Fibonacci
+n=as.integer(readline(prompt="Enter the value of no of elements upto which you wish to calculate fibbonacci sequence"))
+fibo<-vector()# initialized a vector
+fibo[1]=0
+fibo[2]=1
+for(i in 3:n)
+  fibo[i]=fibo[i-1]+fibo[i-2]
+print(fibo)
+
+#OR
+n=as.integer(readline(prompt="Enter the value of no of elements upto which you wish to calculate fibbonacci sequence"))
+n1=0
+n2=1
+if(n<=0)
+  print("value of n is not positive")else{
+  if(n==1)
+    print(n1)else{
+      count=2
+      print(n1)
+      print(n2)
+      while(count<n)
+      {
+        n3=n1+n2
+        print(n3)
+        n1=n2
+        n2=n3
+        count=count+1
+        
+      }
+    }
+}
+
