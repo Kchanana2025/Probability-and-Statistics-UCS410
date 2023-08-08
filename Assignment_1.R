e1cbc78887b511d830d51517398a9ff93bdfcfc2
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-08 13:50:30 +0530

    Create Assignment_1.R

---
+++ Assignment_1.R
@@ -0,0 +1,8 @@
+#Q1
+c<-c(5,10,15,20,25,30)
+mx<-max(c)
+mn<-min(c)
+print(mx)#for normal printing of variable
+print(mn)
+print(paste("MAX is value is",mx))#for printing text as well as value of variable
+print(paste("MIN is value is",mn))
