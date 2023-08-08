0e96b91f382dbc457d73625a78751618ed3081e9
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-08 13:33:08 +0530

    Create basics.R

---
+++ basics.R
@@ -0,0 +1,8 @@
+vec<-c(1,4,5,8) #declaration of vector
+v[2] #accessing 2nd element of vector
+vec[2]
+vec[1:3] #printing all elements from index 1 to 3
+vec1<-1:19 # initialization vector with integers 1 to 19
+vect1[17] #wrong statement vect 1 is not declared
+vec1[17] # 17 is printed on console
+vec1[19] #17 is printed on console
