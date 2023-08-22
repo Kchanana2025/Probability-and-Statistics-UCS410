8cc6425aea419f30c90158ff40fac2fde0cbae7e
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-22 12:36:14 +0530

    Update Assignment_2.R

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,40 +1,93 @@
 #Q1
-n=23#no of persons whos birthday are supposed to be on same day
+
+#Q2
+n<-23#no of persons whos birthday are supposed to be on same day
 prod=1
 
 #prod gives the probability of n persons have birthday on different days
 for(i in 1:(n-1))
-  prod=prod*(1-i/365)
-#prob gives the probability of n persons have birthday on same days
-prob=1-prod
-print(prob)
+  prod<-prod*(1-i/365)
+#ans gives the probability of n persons have birthday on same days
+ans<-1-prod
+print(ans)
 
 #b)
-n=2
+n<-2
 while(n>=2){ 
-  prod=1
+  prod<-1
 for(i in 1:(n-1)){ 
-  prod=prod*(1-(i/365))
+  prod<-prod*(1-(i/365))
 }
-prob=1-prod
+prob<-1-prod
 print(prob)
 if(prob>0.5){
   break
 }
-n=n+1;
+n<-n+1;
 }
 print(n)
 
+#Q3
+calc<-function(pc,pr,pcr)
+{
+  prc<-pcr*pr/pc
+  return (prc)#these btackets are compulsary
+}
+pc<-0.4
+pr<-0.2
+pcr<-0.85
+prob<-calc(pc,pr,pcr)
+print(prob)
+
+#lets send these values through values of vector
+calc<-function(pc,pr,pcr)
+{
+  prc<-pcr*pr/pc
+  return (prc)#these btackets are compulsary
+}
+v<-c(0.4,0.2,0.85)
+pc<-v[1]
+pr<-v[2]
+pcr<-v[3]
+prob<-calc(pc,pr,pcr)
+print(prob)
+
+#lets try send vector instead of values in the function
+calc<-function(pc,pr,pcr)
+{
+  prc<-pcr*pr/pc
+  return (prc)#these brackets are compulsory
+}
+pc<-c(0.4,0.3)#it is a case sensitive language c must be small
+pr<-c(0.2,0.4)
+pcr<-c(0.85,0.75)
+prob<-calc(pc,pr,pcr)
+print(prob)
 #Q4
 data<-iris #iris is inbuilt data set
 head(data)#did just for fun
+
 head(data,10)
 str(data)
-range(data$Sepal.Length)
+range(data$Sepal.Length)#$ is used for specifying column name
 mean(data$Sepal.Length)
 median(data$Sepal.Length)
-quantile(data$Sepal.Length,1)#0.25 is for first quartile ,0.5 for second,0.75 for third,1 for 4th
+quantile(data$Sepal.Length,0.25)#0.25 is for first quartile ,0.5 for second,0.75 for third,1 for 4th
+#waise we are calculating quartile but is named quantile here
+quantile(data$Sepal.Length,0.75)
+IQR(data$Sepal.Length)
 
 #Q5you cant use median mean to do this swal
 #mtlb bina uske bhi krna aana chahie
+
+
+
+#note
+##Delete x (if it exists)
+
+rm(x)
+
+ mean(x=1:10) #[1] 5.5
+
+ x #Error: object 'x' not found
 
